71b880372ac9305512b1fcafa3f194d0b0a75e29
cd <carmendukes@gmail.com>
2020-11-07 12:55:00 -0800

    update api endpoint

--- src/feconfig.js
+++ src/feconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   //API_ENDPOINT: "http://localhost:8000",
-  API_ENDPOINT: "https://cryptic-sierra-63319.herokuapp.com/",
+  API_ENDPOINT: "https://carmen-noteful.herokuapp.com",
   API_KEY: process.env.REACT_APP_API_KEY,
 };
